b4d2ff99b200f657df1d35840c55c6d9aa07d796
HwangWonGyu <hwksdream@naver.com>
2021-06-24 12:22:36 +0900

    단뱡향 연관관계 정리내용

---
+++ src/main/java/hellojpa/JpaOneWayRelationMain.java
@@ -0,0 +1,98 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaOneWayRelationMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
+
+            MemberRelation memberRelation = new MemberRelation();
+            memberRelation.setUsername("member1");
+            memberRelation.setTeam(team); // Team 객체를 MemberRelation 객체가 참조
+            em.persist(memberRelation);
+
+            MemberRelation findMemberRelation = em.find(MemberRelation.class, memberRelation.getId());
+            Team findTeam = findMemberRelation.getTeam(); // teamId 없이도 곧바로 Team 객체 획득 가능
+            System.out.println("findTeam = " + findTeam.getName());
+
+            Team newTeam = em.find(Team.class, 100L); // 100번 키가 DB에 있다고 가정을 하고 Team을 가져와보자.
+            findMemberRelation.setTeam(newTeam); // 그리고나서 찾아둔 MemberRelation의 Team을 변경하면 foreign key가 변경이 된다.
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        /*
+        --출력 결과--
+        Hibernate:
+
+            drop table if exists Member CASCADE
+        Hibernate:
+
+            drop table if exists MemberRelation CASCADE
+        Hibernate:
+
+            drop table if exists Team CASCADE
+        Hibernate:
+
+            drop sequence if exists hibernate_sequence
+        Hibernate: create sequence hibernate_sequence start with 1 increment by 1
+        Hibernate:
+
+            create table Member (
+                id bigint not null,
+                age integer,
+                createdDate timestamp,
+                description clob,
+                lastModifiedDate timestamp,
+                roleType varchar(255),
+                name varchar(255),
+                primary key (id)
+            )
+        Hibernate:
+
+            create table MemberRelation (
+                MEMBER_ID bigint not null,
+                USERNAME varchar(255),
+                TEAM_ID bigint,
+                primary key (MEMBER_ID)
+            )
+        Hibernate:
+
+            create table Team (
+                TEAM_ID bigint not null,
+                name varchar(255),
+                primary key (TEAM_ID)
+            )
+
+        Hibernate:
+
+            alter table MemberRelation
+                add constraint FK574sx72orjbgr84uavmsbtmgp
+                foreign key (TEAM_ID)
+                references Team
+        ===================
+        */
+
+        emf.close();
+    }
+
+}
---
+++ src/main/java/hellojpa/MemberRelation.java
@@ -0,0 +1,42 @@
+package hellojpa;
+
+import javax.persistence.*;
+
+@Entity
+public class MemberRelation {
+
+    @Id @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    @Column(name = "USERNAME")
+    private String username;
+
+    @ManyToOne // MemberRelation 입장에서는 Many, Team 입장에서는 One. [N:1]
+    @JoinColumn(name = "TEAM_ID") // foreign key랑 매핑해야해서 JoinColumn 사용
+    private Team team;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+}
